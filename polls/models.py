f17c2e1fb100cbfcbb9c265ba7c958e7de522f5a
Jullaphong JIAMWATTHANALOET <jullaphong03@gmail.com>
2023-09-02 13:44:00 +0700

    Add docstring comments to models.py

--- polls/models.py
+++ polls/models.py
@@ -6,10 +6,16 @@ from django.contrib import admin
 
 
 class Question(models.Model):
+    """
+    Question Model represents a question with its text and publication date.
+    """
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
+        """
+        Returns a string that represents the text of the question.
+        """
         return self.question_text
 
     @admin.display(
@@ -18,14 +24,27 @@ class Question(models.Model):
         description='Published recently?',
     )
     def was_published_recently(self):
+        """
+        Check if the question was published recently (within the last day).
+
+        :return: True for questions that was published within the last day,
+                 False otherwise.
+        """
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
+    """
+    Choice Model represents a choice in a question with its text
+    and votes count.
+    """
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
+        """
+        Returns a string that represents the text of the choice.
+        """
         return self.choice_text
