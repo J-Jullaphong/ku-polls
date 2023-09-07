b52900517fe4a5a73c8f0945a7dc9f490090f4ba
Jullaphong JIAMWATTHANALOET <jullaphong03@gmail.com>
2023-09-07 21:39:18 +0700

    Implement redirecting from incorrect pages to the polls index page

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, Http404
+from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.views import generic
+from django.contrib import messages
 from django.utils import timezone
 
 from .models import Choice, Question
@@ -30,18 +31,50 @@ class DetailView(generic.DetailView):
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
 
+    def get(self, request, *args, **kwargs):
+        try:
+            question = get_object_or_404(Question, pk=kwargs['pk'])
+        except Http404:
+            messages.error(request, message=f"Poll {kwargs['pk']} not found.")
+            return redirect('polls:index')
+        else:
+            if question.can_vote():
+                return render(request, self.template_name,
+                              {'question': question})
+            else:
+                messages.error(request,
+                               message=f"Poll {kwargs['pk']} is not available "
+                                       f"for voting.")
+                return redirect('polls:index')
+
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
+    def get(self, request, *args, **kwargs):
+        try:
+            question = get_object_or_404(Question, pk=kwargs['pk'])
+        except Http404:
+            messages.error(request, message=f"Poll {kwargs['pk']} not found.")
+            return redirect('polls:index')
+        else:
+            if question.is_published():
+                return render(request, self.template_name,
+                              {'question': question})
+            else:
+                messages.error(request,
+                               message=f"Poll {kwargs['pk']}'s result is not "
+                                       f"available.")
+                return redirect('polls:index')
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
@@ -49,8 +82,5 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,
                                                                    )))
