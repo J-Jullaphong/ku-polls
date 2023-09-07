ffb976b03ca7b914f05659cd848e944f09a9c449
Jullaphong JIAMWATTHANALOET <jullaphong03@gmail.com>
2023-09-07 14:26:24 +0700

    Add end_date and 2 additional methods to the Question class

--- polls/models.py
+++ polls/models.py
@@ -7,10 +7,13 @@ from django.contrib import admin
 
 class Question(models.Model):
     """
-    Question Model represents a question with its text and publication date.
+    Question Model represents a question with its text,
+    publication and end dates.
     """
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    end_date = models.DateTimeField('date ended', default=None, null=True,
+                                    blank=True)
 
     def __str__(self):
         """
@@ -32,6 +35,28 @@ class Question(models.Model):
         """
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def is_published(self):
+        """
+        Check if the question is published.
+
+        :return: True for questions that is published, False otherwise.
+        """
+        now = timezone.now()
+        return self.pub_date <= now
+
+    def can_vote(self):
+        """
+        Check if the question allows voting at a current time based on
+        the publication date and end date (if any).
+
+        :return: True for questions that allows voting at a current time,
+                 False otherwise.
+        """
+        now = timezone.now()
+        if self.end_date is not None:
+            return self.pub_date <= now <= self.end_date
+        return self.pub_date <= now
 
 
 class Choice(models.Model):
