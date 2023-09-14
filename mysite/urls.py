4b259d9eedeb017b5bce1907bbeb72414ce8bdcb
Jullaphong JIAMWATTHANALOET <jullaphong03@gmail.com>
2023-09-14 22:17:12 +0700

    Implement registration(signup)

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
 
+from .views import signup
+
 urlpatterns = [
     path('', RedirectView.as_view(url='/polls/')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('signup/', signup, name='signup')
 ]
